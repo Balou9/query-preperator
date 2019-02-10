21dbc6291212e61cb6382ea39be424ff8de775ca
Balou9 <emmanuel.owosekun@gmail.com>
2019-02-10 10:41:31 +0100

    another

--- test.js
+++ test.js
@@ -5,8 +5,10 @@ tape('queryPreperator - brackets - true', function (t) {
   queryPreperator('./some.txt', 'brackets', false, function (err, data) {
 
     function isbrackets (current) {
-      return /^,?\[.*\]$/.test(current)
+      return /^[,]?\[.*\]$/.test(current)
     }
+
+    console.log(data.split('\r\n').every(isbrackets))
 
     if (err) t.end(err)
     t.true(data, 'true')
@@ -19,7 +21,7 @@ tape('queryPreperator - double quotes - true', function (t) {
   queryPreperator('./some.txt', 'double quotes', false, function (err, data) {
 
     function isdoublequoted (current) {
-      return /^,?\".*\"$/.test(current)
+      return /^[,]?\".*\"$/.test(current)
     }
 
     if (err) t.end(err)
