f7e7d0d9bd00a951eb1bce2514c8419cb1b4b2ea
Balou9 <emmanuel.owosekun@gmail.com>
2019-02-13 12:05:55 +0100

    rm garbage

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 var fs = require('fs')
 
-function prepper (file) {
-  fs.readFile(file, function (err, data) {
-    if (err) throw err
-  })
-}
-
 function queryPreperator (file, type, save, cb) {
   if (typeof file != 'string') throw new TypeError('file should be string')
   if (!(type == 'brackets' || type == 'double quotes')) throw new TypeError('available types')
